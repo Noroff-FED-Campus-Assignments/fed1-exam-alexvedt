619b4b50020323ef8aeda2f5e784800ee77d1459
alexvedt <alexvedt@gmail.com>
2023-05-16 12:48:28 +0200

    major-update

--- js/app.js
+++ js/app.js
@@ -1,3 +1,57 @@
+const env = {
+  BASE_ID: "app3aw9WCV8zgt57M",
+  TABLE_NAME: "test-exam",
+  API_KEY: "keyNDk1zmGMyyUPFh",
+};
+
+const results = document.querySelector("#results");
+
+const BASE_ID = env.BASE_ID;
+const TABLE_NAME = env.TABLE_NAME;
+const apiKey = env.API_KEY;
+
+const tableUrl = `https://api.airtable.com/v0/${BASE_ID}/${TABLE_NAME}`;
+const headers = {
+  Authorization: `Bearer ${apiKey}`,
+  "Content-Type": "application/json",
+};
+
+const getBlogPosts = async () => {
+  try {
+    const response = await fetch(tableUrl, { headers: headers });
+    if (response.ok) {
+      const data = await response.json();
+      results.innerHTML = "";
+      data.records.forEach((blogpost) => {
+        const imageUrl = blogpost.fields["Hero image"][0].thumbnails.large.url;
+        const title = blogpost.fields.Title;
+        const text = blogpost.fields.Text;
+        const createdDate = new Date(blogpost.createdTime).toLocaleDateString();
+
+        results.innerHTML += `
+          <div class="blog-box animate__animated animate__bounceInUp">
+            <div class="blog-img card">
+              <img src="${imageUrl}" alt="blog image">
+            </div>
+            <div class="blog-text">
+            <span>${createdDate} / Theme</span>
+            <a href="#" class="blog-title">${title}</a>
+              <p>${text}</p>
+              <a href="#">Read More</a>
+            </div>
+          </div>
+        `;
+      });
+    } else {
+      console.log("Failed to fetch blog posts");
+    }
+  } catch (err) {
+    console.log("An error occurred:", err);
+  }
+};
+
+getBlogPosts();
+
 /*
 ============================================
 Constants
